31874851e949311ed6d58fd597020d3cc933efdb
JXY3511088jxy <765101769@qq.com>
2019-10-12 18:25:27 +0800

    mybatis案例-20191012

--- src/com/how2java/TestMybatis.java
+++ src/com/how2java/TestMybatis.java
@@ -9,18 +9,23 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import com.how2java.pojo.Category;
+import com.how2java.pojo.Product;
+
 public class TestMybatis {
+
     public static void main(String[] args) throws IOException {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession session=sqlSessionFactory.openSession();
+        SqlSession session = sqlSessionFactory.openSession();
 
-        List<Category> cs=session.selectList("listCategory");
-        for (Category c : cs) {
-            System.out.println(c.getName());
+        List<Product> ps = session.selectList("listProduct");
+        for (Product p : ps) {
+            System.out.println(p+" 对应的分类是 \t "+ p.getCategory());
         }
+
+        session.commit();
+        session.close();
 
     }
 }
